49956a4c151188fdc40303fa528f308648236da3
michelD-dev <delaunaymichel@hotmail.fr>
2022-09-03 18:32:39 +0200

    Implémentation test Bill Image Modal Display

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,16 +1,16 @@
 /**
  * @jest-environment jsdom
  */
-import { fireEvent, screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor, within } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import mockedStore from "../__mocks__/store";
 import router from "../app/Router.js";
-import NewBillUI from "../views/NewBillUI.js";
-import NewBill from "../containers/NewBill.js";
 import Bills from "../containers/Bills.js";
+
+jest.mock("../app/store", () => mockedStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -42,8 +42,8 @@ describe("Given I am connected as an employee", () => {
         .map(a => a.innerHTML);
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
-      console.log(dates);
-      console.log(datesSorted);
+      // console.log(dates);
+      // console.log(datesSorted);
       expect(dates).toEqual(datesSorted);
     });
     //TODO 6
@@ -65,7 +65,7 @@ describe("Given I am connected as an employee", () => {
         const bills = new Bills({
           document,
           onNavigate,
-          store: null,
+          store: mockedStore,
           localStorage: window.localStorage,
         });
 
@@ -78,7 +78,51 @@ describe("Given I am connected as an employee", () => {
         fireEvent.click(buttonNewBill);
         expect(handleClickNewBill).toHaveBeenCalled();
       });
-    }
-    );
+    });
+    describe("When I click on one eye icon", () => {
+      test("I should open a modal displaying the uploaded image file", () => {
+        const onNavigate = pathname => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+
+        Object.defineProperty(window, "localStorage", {
+          value: localStorageMock,
+        });
+
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+        );
+
+        const billPage = new Bills({
+          document,
+          onNavigate,
+          store: mockedStore,
+          localStorage: window.localStorage,
+        });
+
+        $.fn.modal = jest.fn();
+
+        document.body.innerHTML = BillsUI({ data: bills });
+
+        const billsTable = screen.getByTestId("tbody");
+        const iconEyes = within(billsTable).getAllByTestId("icon-eye");
+
+        const handleClickIconEye = jest.fn(icon =>
+          billPage.handleClickIconEye(icon)
+        );
+        iconEyes.forEach(iconEye => {
+          iconEye.addEventListener("click", () => handleClickIconEye(iconEye));
+          fireEvent.click(iconEye);
+        });
+        expect(handleClickIconEye).toHaveBeenCalled();
+        expect(screen.getByText("Justificatif")).toBeVisible()
+      });
+    });
+    describe("When I click on one eye icon", () => {
+      test.todo("I should open a modal displaying the uploaded image file");
+    });
   });
 });
